04344853b3f603a1ef478711049afcada511de4b
Adrian Trilling <adrian.trilling@gmail.com>
2023-05-07 02:50:05 -0400

    testing changing hello function

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,7 @@
 const handler = async function (event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Functions are working. test" })
+        body: JSON.stringify({ message: "Functions are working. test 2:49" })
     };
 };
 
