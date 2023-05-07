d20a3212f9e932e264cccdd0dc115ad696a7ab89
Adrian Trilling <adrian.trilling@gmail.com>
2023-05-07 01:38:45 -0400

    made linter happy

--- netlify/functions/park_search.js
+++ netlify/functions/park_search.js
@@ -7,9 +7,9 @@ const handler = async function (event, context) {
     
     const limit = 600;
 
-    console.log("text: ", text)
-    console.log("process.env.MEILI_URL: ", process.env.MEILI_URL)
-    console.log("process.env.MEILI_SEARCH_KEY: ", process.env.MEILI_SEARCH_KEY)
+    // console.log("text: ", text)
+    // console.log("process.env.MEILI_URL: ", process.env.MEILI_URL)
+    // console.log("process.env.MEILI_SEARCH_KEY: ", process.env.MEILI_SEARCH_KEY)
 
     // https://www.meilisearch.com/docs/reference/api/search
     const query = new URLSearchParams({
@@ -22,18 +22,23 @@ const handler = async function (event, context) {
             // the reason why i used the search key in favor of the master key was that if i made some mistake here and somehow granted users access to this key, 
             // users would only gain further access to read permissions but not write permissions (as they would gain if the master key was exposed).
             headers: {
-                'Authorization': `Bearer ${process.env.MEILI_SEARCH_KEY}`
+                "Authorization": `Bearer ${process.env.MEILI_SEARCH_KEY}`
             }
         })
         .then(response => response.json()
         .then(data => {
-            console.log("data: ", data)
+            // console.log("data: ", data)
             return {
                 statusCode: response.status,
                 body: JSON.stringify({ data: data })
-            }
-        })
-        )
+            };
+        }).catch(err => {
+            console.log("err: ", err);
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ err_message: err.message })
+            };
+    }));
 
                 
             
@@ -44,17 +49,11 @@ const handler = async function (event, context) {
         //     body: JSON.stringify({ data: response })
         // }
     } catch (err) {
-        console.log(err);
+        console.log("err: ", err);
         return {
             statusCode: 500,
             body: JSON.stringify({ err_message: err.message })
         };
-    }
-
-
-    return {
-        statusCode: 200,
-
     }
 };
 
